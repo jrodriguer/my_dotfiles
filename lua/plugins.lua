afbc6bca55f2811c5823c1aa177a3a930e05606b
Julio <juliorodriguezramirez@gmail.com>
2023-11-13 16:29:16 +0100

    reorder plugins and install more

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,4 @@
 local status, packer = pcall(require, "packer")
-
 if (not status) then
   print("Packer is not installed")
   return
@@ -13,19 +12,19 @@ packer.startup(function(use)
     'svrana/neosolarized.nvim',
     requires = { 'tjdevries/colorbuddy.nvim' }
   }
-  use 'nvim-lualine/lualine.nvim' -- Statusline
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'neovim/nvim-lspconfig' -- LSP
+  use 'nvim-lualine/lualine.nvim'       -- Statusline
+  use 'nvim-lua/plenary.nvim'           -- Common utilities
+  use 'onsails/lspkind-nvim'            -- vscode-like pictograms
+  use 'hrsh7th/cmp-buffer'              -- nvim-cmp source for buffer words
+  use 'hrsh7th/cmp-nvim-lsp'            -- nvim-cmp source for neovim's built-in LSP
+  use 'hrsh7th/nvim-cmp'                -- Completion
+  use 'neovim/nvim-lspconfig'           -- LSP
   use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
 
   use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'L3MON4D3/LuaSnip' -- Snippet
+  use 'L3MON4D3/LuaSnip'     -- Snippet
   use {
     'nvim-treesitter/nvim-treesitter',
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
@@ -48,18 +47,20 @@ packer.startup(function(use)
   })
 
   use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
+  use 'dinhhuy258/git.nvim'        -- For git blame & browse
 
   use('MunifTanjim/prettier.nvim') --Prettier plugin for Neovim's built-in
 
   use {
     'akinsho/flutter-tools.nvim',
     requires = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- For vim.ui.select
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- For vim.ui.select
     },
   }
 
-  use {'neoclide/coc.nvim', branch = 'release'}
-  use { "ibhagwan/fzf-lua"}
-end) 
+  use { 'neoclide/coc.nvim', branch = 'release' }
+  use { "ibhagwan/fzf-lua" }
+
+  use 'Bekaboo/deadcolumn.nvim'
+end)
