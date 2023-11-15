4dfeeb27f7551ac948dd54216de198cfe88d5f5d
Julio <juliorodriguezramirez@gmail.com>
2023-11-15 11:43:26 +0100

    remove prettier options

--- after/plugin/prettier.rc.lua
+++ after/plugin/prettier.rc.lua
@@ -15,24 +15,4 @@ prettier.setup({
     "yaml",
     "lua"
   },
-  cli_options = {
-    arrow_parens = "always",
-    bracket_spacing = true,
-    bracket_same_line = false,
-    embedded_language_formatting = "auto",
-    end_of_line = "lf",
-    html_whitespace_sensitivity = "css",
-    jsx_bracket_same_line = true,
-    jsx_single_quote = true,
-    print_width = 80,
-    prose_wrap = "preserve",
-    quote_props = "as-needed",
-    semi = false,
-    single_attribute_per_line = false,
-    single_quote = true,
-    tab_width = 2,
-    trailing_comma = "es5",
-    use_tabs = true,
-    vue_indent_script_and_style = true,
-  }
 })
